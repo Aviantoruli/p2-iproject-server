0f1d740a6b251134eb0a0dc2b8e504a8aafcd81e
Aviantoruli <aviantoandriano@gmail.com>
2021-09-23 07:32:30 +0700

    node mailer done

---
+++ nodemailer/server.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer")
+
+class SendMail {
+    static send(email, pass) {
+        console.log(email, pass)
+        let mailTransporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: "hackshoe7@gmail.com",
+                pass: "eyeshield21"
+            }
+        })
+
+        let mailDetails = {
+            from: "hackshoe7@gmail.com",
+            to: email,
+            subject: "HackShoe",
+            text: "terima kasih telah berbelanja di toko kami, kami akan memproses pesanan anda"
+        }
+
+        // mail sent code
+        return mailTransporter.sendMail(mailDetails, function (err, data) {
+            let error = err ? "Error Occurs" : "Email sent successfully"
+            console.log(error);
+        })
+    }
+}
+
+module.exports = SendMail
